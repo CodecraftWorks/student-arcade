319af6415238705cca7ebc2e0031859897e4cf5a
DESKTOP-R7BR4K0\Admin <drodda@codecraftworks.com>
2017-06-09 18:38:45 -0400

    Added first week of camp 2017 Level Up html projects.

---
+++ camp/2017/level-up/session-a/Pangamon Jump/offline.js
@@ -0,0 +1,32 @@
+﻿{
+	"version": 1497044849,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/pangachoo-sheet0.png",
+		"images/pangachoo-sheet1.png",
+		"images/backround-sheet0.png",
+		"images/tiles-sheet0.png",
+		"images/tiles-sheet1.png",
+		"images/tiles-sheet2.png",
+		"images/backround2-sheet0.png",
+		"images/backdrop-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"images/enemy-sheet0.png",
+		"images/enemy-sheet1.png",
+		"images/edgemarker-sheet0.png",
+		"images/background-sheet0.png",
+		"images/lightning-sheet0.png",
+		"images/fireball-sheet0.png",
+		"images/fireball-sheet1.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
---
+++ camp/2017/level-up/session-a/Platformer/offline.js
@@ -0,0 +1,30 @@
+﻿{
+	"version": 1497044954,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/background3-sheet0.png",
+		"images/player-sheet0.png",
+		"images/tile-sheet0.png",
+		"images/tile-sheet1.png",
+		"images/sprite-sheet0.png",
+		"images/pandabox-sheet0.png",
+		"images/enemy-sheet0.png",
+		"images/edgemarker-sheet0.png",
+		"images/background4-sheet0.png",
+		"images/background5-sheet0.png",
+		"images/background6-sheet0.png",
+		"images/laser-sheet0.png",
+		"images/background7-sheet0.png",
+		"images/panda-sheet0.png",
+		"images/theend-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
