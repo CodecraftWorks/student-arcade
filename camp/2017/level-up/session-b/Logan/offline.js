b69259c537cc6c94bf5e4fce9273af6a67046f96
DESKTOP-R7BR4K0\Admin <drodda@codecraftworks.com>
2017-06-23 14:07:52 -0400

    all games but jivahn's?

---
+++ camp/2017/level-up/session-b/Logan/offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1498241125,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/backround-sheet0.png",
+		"images/purpleguy-sheet0.png",
+		"images/purpleguy-sheet1.png",
+		"images/sprite2-sheet0.png",
+		"images/playerbox-sheet0.png",
+		"images/levelend-sheet0.png",
+		"images/portal-sheet0.png",
+		"images/sprite7-sheet0.png",
+		"images/sprite8-sheet0.png",
+		"images/sprite9-sheet0.png",
+		"images/sprite10-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
---
+++ camp/2017/level-up/session-b/Maya/offline.js
@@ -0,0 +1,24 @@
+﻿{
+	"version": 1498241217,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/background-sheet0.png",
+		"images/mindy-sheet0.png",
+		"images/girl-sheet0.png",
+		"images/girl2-sheet0.png",
+		"images/tile-sheet0.png",
+		"images/tile-sheet1.png",
+		"images/tile-sheet2.png",
+		"images/edgdetecter-sheet0.png",
+		"images/levelend-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
