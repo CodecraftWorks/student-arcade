e4751945b7ab1a2f1926ca4ffda83a1392c96491
DESKTOP-R7BR4K0\Admin <drodda@codecraftworks.com>
2017-07-14 18:44:34 -0400

    Alyssa's project

---
+++ camp/2017/Alyssa-Asteroids/offline.js
@@ -0,0 +1,28 @@
+﻿{
+	"version": 1500072199,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/background-sheet0.png",
+		"images/player-sheet0.png",
+		"images/forwardthrusters-sheet0.png",
+		"images/forwardthrusters-sheet1.png",
+		"images/forwardthrusters-sheet2.png",
+		"images/reversethruster-sheet0.png",
+		"images/reversethruster-sheet1.png",
+		"images/asteroid-sheet0.png",
+		"images/starterbackground-sheet0.png",
+		"images/laser-sheet0.png",
+		"images/lifebar-sheet0.png",
+		"images/lifebarborder-sheet0.png",
+		"images/lifebar2.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
