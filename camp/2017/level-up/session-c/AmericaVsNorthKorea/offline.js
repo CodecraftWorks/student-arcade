3ca5807beecf4c1dc680b2cc74813373f177da23
DESKTOP-R7BR4K0\Admin <drodda@codecraftworks.com>
2017-07-14 18:51:04 -0400

    adding mitch's project

---
+++ camp/2017/level-up/session-c/AmericaVsNorthKorea/offline.js
@@ -0,0 +1,27 @@
+﻿{
+	"version": 1500072589,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/background-sheet0.png",
+		"images/player-sheet0.png",
+		"images/thrustersforward-sheet0.png",
+		"images/thrustersforward-sheet1.png",
+		"images/thrustersforward-sheet2.png",
+		"images/reversthrust-sheet0.png",
+		"images/reversthrust-sheet1.png",
+		"images/countclockwisetopleft-sheet0.png",
+		"images/countclockwisetopleft-sheet1.png",
+		"images/electricity-sheet0.png",
+		"images/enemy-sheet0.png",
+		"images/decoration-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
