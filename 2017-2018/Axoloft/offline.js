d7f23ba7388185b9c8ae9fbef8cadb2971412396
dustinRodda <dustinrodda@gmail.com>
2017-11-11 09:51:38 -0500

    adding graeme's and emmanuel's projects for b&n

---
+++ 2017-2018/Axoloft/offline.js
@@ -0,0 +1,29 @@
+﻿{
+	"version": 1510410855,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/player-sheet0.png",
+		"images/background-sheet0.png",
+		"images/food-sheet0.png",
+		"images/place-sheet0.png",
+		"images/enemy-sheet0.png",
+		"images/bullet-sheet0.png",
+		"images/goal-sheet0.png",
+		"images/enemy2-sheet0.png",
+		"images/hitsreen-sheet0.png",
+		"images/hi-sheet0.png",
+		"images/moreammo-sheet0.png",
+		"images/goal2-sheet0.png",
+		"images/goalspawn-sheet0.png",
+		"images/background2-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
