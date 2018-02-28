e8b300feaa3071c254862ea235f1b02fa10c9b0d
dustinRodda <dustinrodda@gmail.com>
2018-02-27 19:09:00 -0500

    Adding Jackson's game!!!!

---
+++ 2017-2018/Jackson-Brickbreaker/offline.js
@@ -0,0 +1,19 @@
+﻿{
+	"version": 1519776312,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/paddle-sheet0.png",
+		"images/ball-sheet0.png",
+		"images/wall-sheet0.png",
+		"images/brick-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
